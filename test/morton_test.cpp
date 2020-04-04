af74261278bedcd9136da62b0f5838fff03c7dfa
Sho Hirose <sho.hirose@gmail.com>
2020-04-05 02:48:44 +0900

    test: Add unit tests for impls using lookup tables without pre-shift

--- test/morton_test.cpp
+++ test/morton_test.cpp
@@ -2,6 +2,11 @@
 
 #include <gtest/gtest.h>
 
+using morton::decode;
+using morton::encode;
+
+namespace tag = morton::tag;
+
 class Morton2d32BitTest : public ::testing::Test {
  protected:
   const uint_fast16_t x_[8] = {0, 1, 2, 3, 4, 5, 6, 7};
@@ -19,22 +24,44 @@ class Morton2d32BitTest : public ::testing::Test {
   };
 };
 
-TEST_F(Morton2d32BitTest, Encoding) {
+TEST_F(Morton2d32BitTest, EncodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 7; ++i) {
     for (int j = 0; j < 7; ++j) {
-      const auto m = morton::encode(x_[j], y_[i]);
+      const auto m = encode(x_[j], y_[i], tag::preshifted_lookup_table{});
       EXPECT_EQ(m, m_[i * 8 + j])
           << "  x = " << x_[j] << ", y = " << y_[i] << '\n';
     }
   }
 }
 
-TEST_F(Morton2d32BitTest, Decoding) {
+TEST_F(Morton2d32BitTest, EncodingUsingLookupTable) {
+  for (int i = 0; i < 7; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      const auto m = encode(x_[j], y_[i], tag::lookup_table{});
+      EXPECT_EQ(m, m_[i * 8 + j])
+          << "  x = " << x_[j] << ", y = " << y_[i] << '\n';
+    }
+  }
+}
+
+TEST_F(Morton2d32BitTest, DecodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 7; ++i) {
     for (int j = 0; j < 7; ++j) {
       uint_fast16_t x, y;
       const auto k = i * 8 + j;
-      morton::decode(m_[k], x, y);
+      decode(m_[k], x, y, tag::preshifted_lookup_table{});
+      EXPECT_EQ(x, x_[j]) << "  m = " << m_[k] << '\n';
+      EXPECT_EQ(y, y_[i]) << "  m = " << m_[k] << '\n';
+    }
+  }
+}
+
+TEST_F(Morton2d32BitTest, DecodingUsingLookupTable) {
+  for (int i = 0; i < 7; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      uint_fast16_t x, y;
+      const auto k = i * 8 + j;
+      decode(m_[k], x, y, tag::lookup_table{});
       EXPECT_EQ(x, x_[j]) << "  m = " << m_[k] << '\n';
       EXPECT_EQ(y, y_[i]) << "  m = " << m_[k] << '\n';
     }
@@ -58,22 +85,44 @@ class Morton2d64BitTest : public ::testing::Test {
   };
 };
 
-TEST_F(Morton2d64BitTest, Encoding) {
+TEST_F(Morton2d64BitTest, EncodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 7; ++i) {
     for (int j = 0; j < 7; ++j) {
-      const auto m = morton::encode(x_[j], y_[i]);
+      const auto m = encode(x_[j], y_[i], tag::preshifted_lookup_table{});
       EXPECT_EQ(m, m_[i * 8 + j])
           << "  x = " << x_[j] << ", y = " << y_[i] << '\n';
     }
   }
 }
 
-TEST_F(Morton2d64BitTest, Decoding) {
+TEST_F(Morton2d64BitTest, EncodingUsingLookupTable) {
+  for (int i = 0; i < 7; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      const auto m = encode(x_[j], y_[i], tag::lookup_table{});
+      EXPECT_EQ(m, m_[i * 8 + j])
+          << "  x = " << x_[j] << ", y = " << y_[i] << '\n';
+    }
+  }
+}
+
+TEST_F(Morton2d64BitTest, DecodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 7; ++i) {
     for (int j = 0; j < 7; ++j) {
       uint_fast32_t x, y;
       const auto k = i * 8 + j;
-      morton::decode(m_[k], x, y);
+      decode(m_[k], x, y, tag::preshifted_lookup_table{});
+      EXPECT_EQ(x, x_[j]) << "  m = " << m_[k] << '\n';
+      EXPECT_EQ(y, y_[i]) << "  m = " << m_[k] << '\n';
+    }
+  }
+}
+
+TEST_F(Morton2d64BitTest, DecodingUsingLookupTable) {
+  for (int i = 0; i < 7; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      uint_fast32_t x, y;
+      const auto k = i * 8 + j;
+      decode(m_[k], x, y, tag::lookup_table{});
       EXPECT_EQ(x, x_[j]) << "  m = " << m_[k] << '\n';
       EXPECT_EQ(y, y_[i]) << "  m = " << m_[k] << '\n';
     }
@@ -82,8 +131,6 @@ TEST_F(Morton2d64BitTest, Decoding) {
 
 class Morton3d32BitTest : public ::testing::Test {
  protected:
-  void SetUp() override {}
-
   const uint_fast16_t x_[4] = {0, 1, 2, 3};
   const uint_fast16_t y_[4] = {0, 1, 2, 3};
   const uint_fast16_t z_[4] = {0, 1, 2, 3};
@@ -116,11 +163,12 @@ class Morton3d32BitTest : public ::testing::Test {
   };
 };
 
-TEST_F(Morton3d32BitTest, Encoding) {
+TEST_F(Morton3d32BitTest, EncodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 4; ++j) {
       for (int k = 0; k < 4; ++k) {
-        const auto m = morton::encode(x_[k], y_[j], z_[i]);
+        const auto m =
+            encode(x_[k], y_[j], z_[i], tag::preshifted_lookup_table{});
         EXPECT_EQ(m, m_[(i * 4 + j) * 4 + k])
             << "  x = " << x_[k] << ", y = " << y_[j] << ", z = " << z_[i]
             << '\n';
@@ -129,13 +177,41 @@ TEST_F(Morton3d32BitTest, Encoding) {
   }
 }
 
-TEST_F(Morton3d32BitTest, Decoding) {
+TEST_F(Morton3d32BitTest, EncodingUsingLookupTable) {
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        const auto m = encode(x_[k], y_[j], z_[i], tag::lookup_table{});
+        EXPECT_EQ(m, m_[(i * 4 + j) * 4 + k])
+            << "  x = " << x_[k] << ", y = " << y_[j] << ", z = " << z_[i]
+            << '\n';
+      }
+    }
+  }
+}
+
+TEST_F(Morton3d32BitTest, DecodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 4; ++j) {
       for (int k = 0; k < 4; ++k) {
         uint_fast16_t x, y, z;
         const auto l = (i * 4 + j) * 4 + k;
-        morton::decode(m_[l], x, y, z);
+        decode(m_[l], x, y, z, tag::preshifted_lookup_table{});
+        EXPECT_EQ(x, x_[k]) << "  m = " << m_[l] << '\n';
+        EXPECT_EQ(y, y_[j]) << "  m = " << m_[l] << '\n';
+        EXPECT_EQ(z, z_[i]) << "  m = " << m_[l] << '\n';
+      }
+    }
+  }
+}
+
+TEST_F(Morton3d32BitTest, DecodingUsingLookupTable) {
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        uint_fast16_t x, y, z;
+        const auto l = (i * 4 + j) * 4 + k;
+        decode(m_[l], x, y, z, tag::lookup_table{});
         EXPECT_EQ(x, x_[k]) << "  m = " << m_[l] << '\n';
         EXPECT_EQ(y, y_[j]) << "  m = " << m_[l] << '\n';
         EXPECT_EQ(z, z_[i]) << "  m = " << m_[l] << '\n';
@@ -178,11 +254,12 @@ class Morton3d64BitTest : public ::testing::Test {
   };
 };
 
-TEST_F(Morton3d64BitTest, Encoding) {
+TEST_F(Morton3d64BitTest, EncodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 4; ++j) {
       for (int k = 0; k < 4; ++k) {
-        const auto m = morton::encode(x_[k], y_[j], z_[i]);
+        const auto m =
+            encode(x_[k], y_[j], z_[i], tag::preshifted_lookup_table{});
         EXPECT_EQ(m, m_[(i * 4 + j) * 4 + k])
             << "  x = " << x_[k] << ", y = " << y_[j] << ", z = " << z_[i]
             << '\n';
@@ -191,13 +268,41 @@ TEST_F(Morton3d64BitTest, Encoding) {
   }
 }
 
-TEST_F(Morton3d64BitTest, Decoding) {
+TEST_F(Morton3d64BitTest, EncodingUsingLookupTable) {
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        const auto m = encode(x_[k], y_[j], z_[i], tag::lookup_table{});
+        EXPECT_EQ(m, m_[(i * 4 + j) * 4 + k])
+            << "  x = " << x_[k] << ", y = " << y_[j] << ", z = " << z_[i]
+            << '\n';
+      }
+    }
+  }
+}
+
+TEST_F(Morton3d64BitTest, DecodingUsingPreshiftedLookupTable) {
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 4; ++j) {
       for (int k = 0; k < 4; ++k) {
         uint_fast32_t x, y, z;
         const auto l = (i * 4 + j) * 4 + k;
-        morton::decode(m_[l], x, y, z);
+        decode(m_[l], x, y, z, tag::preshifted_lookup_table{});
+        EXPECT_EQ(x, x_[k]) << "  m = " << m_[l] << '\n';
+        EXPECT_EQ(y, y_[j]) << "  m = " << m_[l] << '\n';
+        EXPECT_EQ(z, z_[i]) << "  m = " << m_[l] << '\n';
+      }
+    }
+  }
+}
+
+TEST_F(Morton3d64BitTest, DecodingUsingLookupTable) {
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      for (int k = 0; k < 4; ++k) {
+        uint_fast32_t x, y, z;
+        const auto l = (i * 4 + j) * 4 + k;
+        decode(m_[l], x, y, z, tag::lookup_table{});
         EXPECT_EQ(x, x_[k]) << "  m = " << m_[l] << '\n';
         EXPECT_EQ(y, y_[j]) << "  m = " << m_[l] << '\n';
         EXPECT_EQ(z, z_[i]) << "  m = " << m_[l] << '\n';
@@ -213,8 +318,8 @@ TEST(MortonBugReportTest, IssueNo25Case) {
     const uint_fast16_t y = 1951;
     const uint_fast16_t z = 975;
     const uint_fast32_t m_correct = 4293967295;
-    const auto m = morton::encode(x, y, z);
-    EXPECT_EQ(m, m_correct);
+    EXPECT_EQ(encode(x, y, z, tag::preshifted_lookup_table{}), m_correct);
+    EXPECT_EQ(encode(x, y, z, tag::lookup_table{}), m_correct);
   }
 
   // 64 Bits
@@ -223,8 +328,8 @@ TEST(MortonBugReportTest, IssueNo25Case) {
     const uint_fast32_t y = 1951;
     const uint_fast32_t z = 975;
     const uint_fast64_t m_correct = 4293967295;
-    const auto m = morton::encode(x, y, z);
-    EXPECT_EQ(m, m_correct);
+    EXPECT_EQ(encode(x, y, z, tag::preshifted_lookup_table{}), m_correct);
+    EXPECT_EQ(encode(x, y, z, tag::lookup_table{}), m_correct);
   }
 
   // 64 Bits
@@ -233,12 +338,16 @@ TEST(MortonBugReportTest, IssueNo25Case) {
     const uint_fast32_t yt = 2097055;
     const uint_fast32_t zt = 2097103;
     const uint_fast64_t mt = 9223372036853775807;
-
-    const auto m = morton::encode(xt, yt, zt);
-    EXPECT_EQ(m, mt);
+    EXPECT_EQ(encode(xt, yt, zt, tag::preshifted_lookup_table{}), mt);
+    EXPECT_EQ(encode(xt, yt, zt, tag::lookup_table{}), mt);
 
     uint_fast32_t x, y, z;
-    morton::decode(mt, x, y, z);
+    decode(mt, x, y, z, tag::preshifted_lookup_table{});
+    EXPECT_EQ(x, xt);
+    EXPECT_EQ(y, yt);
+    EXPECT_EQ(z, zt);
+
+    decode(mt, x, y, z, tag::lookup_table{});
     EXPECT_EQ(x, xt);
     EXPECT_EQ(y, yt);
     EXPECT_EQ(z, zt);

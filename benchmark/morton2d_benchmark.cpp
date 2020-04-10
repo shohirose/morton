8c78f08130f1507b8bf7aaa37359a7cdcd96f0fb
Sho Hirose <sho.hirose@gmail.com>
2020-04-10 23:10:58 +0900

    feat: Create benchmarks

---
+++ benchmark/morton2d_benchmark.cpp
@@ -0,0 +1,76 @@
+#include <benchmark/benchmark.h>
+
+#include <random>
+#include <vector>
+
+#include "morton/morton2d.hpp"
+
+using namespace morton2d;
+
+template <typename Engine, typename T, typename Tag>
+void RunBenchmark(benchmark::State& state) {
+  std::random_device seed_gen;
+  Engine engine(seed_gen());
+  std::vector<coordinates<T>> coords(state.range(0));
+
+  for (auto _ : state) {
+    state.PauseTiming();
+    for (auto&& c : coords) {
+      c.x = static_cast<T>(engine());
+      c.y = static_cast<T>(engine());
+    }
+    state.ResumeTiming();
+    for (int i = 0; i < state.range(0); ++i) {
+      const auto c = encode(coords[i], Tag{});
+      benchmark::DoNotOptimize(c);
+    }
+  }
+}
+
+static void BM_32BitEncodingUsingPreshiftedLookupTable(
+    benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, tag::preshifted_lookup_table>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingPreshiftedLookupTable)->Range(8, 8 << 10);
+
+static void BM_32BitEncodingUsingLookupTable(benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, tag::lookup_table>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingLookupTable)->Range(8, 8 << 10);
+
+static void BM_32BitEncodingUsingMagicBits(benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, tag::magic_bits>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingMagicBits)->Range(8, 8 << 10);
+
+#ifdef MORTON2D_USE_BMI
+static void BM_32BitEncodingUsingBmi(benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, tag::bmi>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingBmi)->Range(8, 8 << 10);
+#endif  // MORTON2d_USE_BMI
+
+static void BM_64BitEncodingUsingPreshiftedLookupTable(
+    benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, tag::preshifted_lookup_table>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingPreshiftedLookupTable)->Range(8, 8 << 10);
+
+static void BM_64BitEncodingUsingLookupTable(benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, tag::lookup_table>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingLookupTable)->Range(8, 8 << 10);
+
+static void BM_64BitEncodingUsingMagicBits(benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, tag::magic_bits>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingMagicBits)->Range(8, 8 << 10);
+
+#ifdef MORTON2D_USE_BMI
+static void BM_64BitEncodingUsingBmi(benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, tag::bmi>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingBmi)->Range(8, 8 << 10);
+#endif  // MORTON2d_USE_BMI
+
+BENCHMARK_MAIN();
---
+++ benchmark/morton3d_benchmark.cpp
@@ -0,0 +1,78 @@
+#include <benchmark/benchmark.h>
+
+#include <random>
+#include <vector>
+#include <limits>
+
+#include "morton/morton3d.hpp"
+
+using namespace morton3d;
+
+template <typename Engine, typename T, int N, typename Tag>
+void RunBenchmark(benchmark::State& state) {
+  std::random_device seed_gen;
+  Engine engine(seed_gen());
+  std::uniform_int_distribution<T> dist(0, (static_cast<T>(1) << N) - 1);
+  std::vector<coordinates<T>> coords(state.range(0));
+
+  for (auto _ : state) {
+    state.PauseTiming();
+    for (auto&& c : coords) {
+      c.x = dist(engine);
+      c.y = dist(engine);
+    }
+    state.ResumeTiming();
+    for (int i = 0; i < state.range(0); ++i) {
+      const auto c = encode(coords[i], Tag{});
+      benchmark::DoNotOptimize(c);
+    }
+  }
+}
+
+static void BM_32BitEncodingUsingPreshiftedLookupTable(
+    benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, 10, tag::preshifted_lookup_table>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingPreshiftedLookupTable)->Range(8, 8 << 10);
+
+static void BM_32BitEncodingUsingLookupTable(benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, 10, tag::lookup_table>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingLookupTable)->Range(8, 8 << 10);
+
+static void BM_32BitEncodingUsingMagicBits(benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, 10, tag::magic_bits>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingMagicBits)->Range(8, 8 << 10);
+
+#ifdef MORTON3D_USE_BMI
+static void BM_32BitEncodingUsingBmi(benchmark::State& state) {
+  RunBenchmark<std::mt19937, uint16_t, 10, tag::bmi>(state);
+}
+BENCHMARK(BM_32BitEncodingUsingBmi)->Range(8, 8 << 10);
+#endif  // MORTON3D_USE_BMI
+
+static void BM_64BitEncodingUsingPreshiftedLookupTable(
+    benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, 21, tag::preshifted_lookup_table>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingPreshiftedLookupTable)->Range(8, 8 << 10);
+
+static void BM_64BitEncodingUsingLookupTable(benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, 21, tag::lookup_table>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingLookupTable)->Range(8, 8 << 10);
+
+static void BM_64BitEncodingUsingMagicBits(benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, 21, tag::magic_bits>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingMagicBits)->Range(8, 8 << 10);
+
+#ifdef MORTON3D_USE_BMI
+static void BM_64BitEncodingUsingBmi(benchmark::State& state) {
+  RunBenchmark<std::mt19937_64, uint32_t, 21, tag::bmi>(state);
+}
+BENCHMARK(BM_64BitEncodingUsingBmi)->Range(8, 8 << 10);
+#endif  // MORTON3D_USE_BMI
+
+BENCHMARK_MAIN();
